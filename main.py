f739f965caeb6fcd3e1f7a01556fcb7330f5fe15
PurpleFumetsu <46788247+PurpleFumetsu@users.noreply.github.com>
2020-06-12 10:25:03 -0700

    made some minor changes

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-#Luke Scatchard
-#5/20/20
-#kamaitachi game (i havent decided what to call it yet)
+# Luke Scatchard
+# 5/20/20
+# jebrjf
 
-import pygame, sys, math, random
+import pygame
+import sys
+import math
+import random
 from pygame.locals import *
 from pygame import gfxdraw
 from PIL import Image
 
 pygame.init()
-fpsClock=pygame.time.Clock()
+fpsClock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((928,534))
+screen = pygame.display.set_mode((928, 534))
 pygame.display.set_caption('jebrjf')
 myfont = pygame.font.SysFont("monospace", 20)
 
@@ -24,15 +27,16 @@ left_pressed = False
 right_pressed = False
 
 bullet_counter = 0
+enemies_killed = 0
 
 spawn_counter = 0
 sawn_rand = 0
 
-
+#checks if a sprite is on the ground
 def OnGround(player):
 
     blocks_hit_list = pygame.sprite.pygame.sprite.spritecollide(player, blocklist, False)
-    
+
     if len(blocks_hit_list) >= 1:
         for block in blocks_hit_list:
             if player.y+50 <= block.y and player.y+64 >= block.y:
@@ -50,9 +54,7 @@ def OnGround(player):
     else:
         return False
 
-def CollectCoins(player):
-    coinlist = pygame.sprite.pygame.sprite.spritecollide(player, coins, True)
-
+#used to draw out all of the things
 def DrawWindow():
     screen.blit(bg_layer_1, (bg_layer_1_x, 0))
     screen.blit(bg_layer_2, (bg_layer_2_x, 213))
@@ -60,20 +62,18 @@ def DrawWindow():
     screen.blit(MAIN_PLAYER.image, (MAIN_PLAYER.x, MAIN_PLAYER.y))
     for i in blocklist:
         screen.blit(i.image, (i.leftx, i.y))
-    for i in coins:
-        #if i.collected == False:
-        screen.blit(i.image, (i.xleft, i.ytop))
     for i in enemylist:
         screen.blit(i.image, (i.x, i.y))
     for i in bulletlist:
         screen.blit(i.image, (i.x, i.y))
     pygame.display.update()
 
+#player class controls the player
 class Player(pygame.sprite.Sprite):
     def __init__(self):
 
         pygame.sprite.Sprite.__init__(self)
-        
+
         self.x = 150
         self.y = 13*32
         self.speed = 0
@@ -88,6 +88,7 @@ class Player(pygame.sprite.Sprite):
 
     def move(self):
 
+        #moves the player
         if left_pressed == True:
             if self.speed >= -3.0:
                  self.speed -= 1.0
@@ -103,35 +104,38 @@ class Player(pygame.sprite.Sprite):
 
         self.state = "air"
 
-        if OnGround(MAIN_PLAYER): 
+        #refills jumpyness when you touch the ground
+        if OnGround(MAIN_PLAYER):
             self.state = "ground"
             self.jumppower = 8
 
         if self.state != "ground":
             self.y -= self.jumppower
             self.jumpcounter += 1
-            #decreases jump power every six frames
-            #raise to make jump floatier and lower to make more tight
+            # decreases jump power every six frames
+            # raise to make jump floatier and lower to make more tight
             if self.jumpcounter > 4:
                 self.jumppower -= 1
                 self.jumpcounter = 0
-
+        
+        #moves player based on speed
         self.x += self.speed
-
         if self.x > 866:
             self.x = 866
         elif self.x < 0:
             self.x = 0
 
+        #updates players collision
         self.rect = pygame.Rect(self.x, self.y, 64, 64)
 
-
     def update(self):
+        #switches images based on direction player is facing
         if right_pressed == True:
             self.image = self.image1
         elif left_pressed == True:
             self.image = self.image2
 
+#the bullet class controls the projectile things
 class bullet(pygame.sprite.Sprite):
     def __init__(self):
 
@@ -147,11 +151,12 @@ class bullet(pygame.sprite.Sprite):
             self.speed = -5
         self.y = MAIN_PLAYER.y
         self.rect = pygame.Rect(self.x, self.y+5, 54, 42)
-    
+
     def move(self):
         self.x += self.speed
         self.rect = pygame.Rect(self.x, self.y+5, 54, 42)
 
+#the enemy class controls the thundermuffins
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, xpos, ypos, speed):
 
@@ -169,8 +174,10 @@ class Enemy(pygame.sprite.Sprite):
         self.y = ypos
         self.rect = pygame.Rect(xpos+3, ypos+19, 58, 42)
         self.imageswitch = 0
-    
+
     def switchimage(self):
+        #switches images a couple times per second
+        #i just think it looks funny
         self.imageswitch += 1
         if self.imageswitch <= 19:
             self.image = self.image1
@@ -181,18 +188,20 @@ class Enemy(pygame.sprite.Sprite):
             self.imageswitch = 0
 
     def move(self):
-
+        
+        # movement is very similar to players except has smaller jumps 
+        # and only switches direction when it hits a wall
         self.state = "air"
 
-        if OnGround(self): 
+        if OnGround(self):
             self.state = "ground"
             self.jumppower = 5
 
         if self.state != "ground":
             self.y -= self.jumppower
             self.jumpcounter += 1
-            #decreases jump power every six frames
-            #raise to make jump floatier and lower to make more tight
+            # decreases jump power every six frames
+            # raise to make jump floatier and lower to make more tight
             if self.jumpcounter > 4:
                     self.jumppower -= 1
                     self.jumpcounter = 0
@@ -202,15 +211,20 @@ class Enemy(pygame.sprite.Sprite):
         if self.x > 866 or self.x < 0:
             self.speed *= -1
 
+        # updates thundermuffin collision
         self.rect = pygame.Rect(self.x+3, self.y+19, 58, 42)
 
     def collisioncheck(self, other):
+        #checks if it is touching something and what it is touching
+        #pretty much if the other thing has the same image as the player
+        #then its the player and if its not its a bullet
         if pygame.sprite.collide_rect(self, other):
             if other.image == MAIN_PLAYER.image:
                 return 'player death'
             else:
                 return 'hit'
 
+#the block class is used for all solid ground
 class Blocks(pygame.sprite.Sprite):
     def __init__(self, xpos, ypos, sprite):
 
@@ -222,77 +236,61 @@ class Blocks(pygame.sprite.Sprite):
         elif sprite == 2:
             self.image = pygame.image.load('blocklooong.png')
             self.rect = pygame.Rect(xpos, ypos, 1820, 64)
-        
+
         self.leftx = xpos
         self.rightx = xpos + self.image.get_width()
         self.y = ypos
         self.blocks_speed = 1.2
 
-class Coin(pygame.sprite.Sprite):
-    def __init__(self, xpos, ypos):
+# sets up and adds blocks to stage
+    blocklist = pygame.sprite.Group()
+    blocklist.add(Blocks(0, 15*32, 2))
 
-        pygame.sprite.Sprite.__init__(self)
+    # middle left
+    blocklist.add(Blocks(0, 10*32, 1))
+    blocklist.add(Blocks(32, 10*32, 1))
+    blocklist.add(Blocks(2*32, 10*32, 1))
+    blocklist.add(Blocks(3*32, 10*32, 1))
 
-        self.xleft = xpos
-        self.xright = xpos+64
-        self.ytop = ypos
-        self.ybtm = ypos+64
-        self.image = pygame.image.load('kkoin.png')
-        self.rect = pygame.Rect(xpos, ypos, 64, 64)
-    
-#adding coins to stage
-coins = pygame.sprite.Group()
-coins.add(Coin(32, 8*32))
-coins.add(Coin(26*32, 8*32))   
+    # middle right
+    blocklist.add(Blocks(28*32, 10*32, 1))
+    blocklist.add(Blocks(27*32, 10*32, 1))
+    blocklist.add(Blocks(26*32, 10*32, 1))
+    blocklist.add(Blocks(25*32, 10*32, 1))
 
-#adding blocks to stage
-blocklist = pygame.sprite.Group()
-blocklist.add(Blocks(0, 15*32, 2))
+    # middle center
+    blocklist.add(Blocks(12*32, 10*32, 1))
+    blocklist.add(Blocks(13*32, 10*32, 1))
+    blocklist.add(Blocks(14*32, 10*32, 1))
+    blocklist.add(Blocks(15*32, 10*32, 1))
+    blocklist.add(Blocks(16*32, 10*32, 1))
+    blocklist.add(Blocks(17*32, 10*32, 1))
 
-#middle left
-blocklist.add(Blocks(0, 10*32, 1))
-blocklist.add(Blocks(32, 10*32, 1))
-blocklist.add(Blocks(2*32, 10*32, 1))
-blocklist.add(Blocks(3*32, 10*32, 1))
+    # top left
+    blocklist.add(Blocks(6*32, 5*32, 1))
+    blocklist.add(Blocks(7*32, 5*32, 1))
+    blocklist.add(Blocks(8*32, 5*32, 1))
+    blocklist.add(Blocks(9*32, 5*32, 1))
 
-#middle right
-blocklist.add(Blocks(28*32, 10*32, 1))
-blocklist.add(Blocks(27*32, 10*32, 1))
-blocklist.add(Blocks(26*32, 10*32, 1))
-blocklist.add(Blocks(25*32, 10*32, 1))
-
-#middle center
-blocklist.add(Blocks(12*32, 10*32, 1))
-blocklist.add(Blocks(13*32, 10*32, 1))
-blocklist.add(Blocks(14*32, 10*32, 1))
-blocklist.add(Blocks(15*32, 10*32, 1))
-blocklist.add(Blocks(16*32, 10*32, 1))
-blocklist.add(Blocks(17*32, 10*32, 1))
-
-#top left
-blocklist.add(Blocks(6*32, 5*32, 1))
-blocklist.add(Blocks(7*32, 5*32, 1))
-blocklist.add(Blocks(8*32, 5*32, 1))
-blocklist.add(Blocks(9*32, 5*32, 1))
-
-#top right
-blocklist.add(Blocks(20*32, 5*32, 1))
-blocklist.add(Blocks(21*32, 5*32, 1))
-blocklist.add(Blocks(22*32, 5*32, 1))
-blocklist.add(Blocks(23*32, 5*32, 1))
-
+    # top right
+    blocklist.add(Blocks(20*32, 5*32, 1))
+    blocklist.add(Blocks(21*32, 5*32, 1))
+    blocklist.add(Blocks(22*32, 5*32, 1))
+    blocklist.add(Blocks(23*32, 5*32, 1))
+# sets up enemy and bullet sprite groups
 enemylist = pygame.sprite.Group()
 bulletlist = pygame.sprite.Group()
 
 MAIN_PLAYER = Player()
 
+#main game loop
 rungame = True
 while rungame:
-
+    #checks which keys youre pressing
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             rungame = False
-        elif event.type == KEYDOWN and event.key == pygame.K_a:
+        if event.type == KEYDOWN and event.key == pygame.K_a:
             left_pressed = True
             right_pressed = False
         elif event.type == KEYDOWN and event.key == pygame.K_d:
@@ -301,30 +299,34 @@ while rungame:
         else:
             left_pressed = False
             right_pressed = False
-        
+
+        #bullet counter is here to prevent players from spamming bullets
         if event.type == KEYDOWN and event.key == pygame.K_SPACE and bullet_counter >= 25:
-            bullet_counter = 0
+            bullet_counter -= 25
             bulletlist.add(bullet())
-    bullet_counter += 1
+    if bullet_counter < 100:
+        bullet_counter += 1
 
-    #screen.blit(track, (0,0))
-    #MAIN_PLAYER.turn(Count)
-
+    #moves the player
     MAIN_PLAYER.move()
     MAIN_PLAYER.update()
-    CollectCoins(MAIN_PLAYER)
+    #moves the bullet things
     for i in bulletlist:
         i.move()
+    #moves the thundermuffins
     for i in enemylist:
         i.switchimage()
         i.move()
         if i.collisioncheck(MAIN_PLAYER) == 'player death':
             rungame = False
+            print("can we get an f in the chat")
         for x in bulletlist:
             if i.collisioncheck(x) == 'hit':
+                enemies_killed += 1
+                print("You've killed " + str(enemies_killed) + " thundermuffins. Murderer.")
                 enemylist.remove(i)
 
-    
+    #has enemies randomly spawn in
     spawn_counter += 1
     if spawn_counter >= 300:
         spawn_rand = random.randint(1, 200)
@@ -335,10 +337,9 @@ while rungame:
             enemylist.add(Enemy(21*32, 3*32, -3))
             spawn_counter = 100
 
-
     DrawWindow()
-
     pygame.display.update()
     fpsClock.tick(60)
 
+    
 
